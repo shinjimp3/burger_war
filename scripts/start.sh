c86b9e54845c1968b931c8aae5b19cc75aa2398e
shinjimp3 <tbw.rbcn.1211129@gmail.com>
2020-02-29 22:30:28 +0900

    change enemy kinds

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,4 +4,5 @@
 bash judge/test_scripts/set_running.sh localhost:5000
 
 # launch robot control node
-roslaunch burger_war sim_robot_run.launch
+#roslaunch burger_war sim_robot_run.launch
+roslaunch burger_war sim_level_1_cheese.launch
